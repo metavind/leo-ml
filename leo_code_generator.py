89ae06b649b533aa1c16a5193d5e8abdaa2868b8
metavind <metavind@gmail.com>
2023-08-21 22:42:15 +0200

    feat: move output indexing to normal layer indexing

--- leo_code_generator.py
+++ leo_code_generator.py
@@ -5,17 +5,18 @@ import json
 def generate_nn_code(program_name, model_data):
     indentation = "    "
 
-    # Infer input_dim and output_dim from model_data
+    # Infer input_dim and total number of layers from model_data
+    idx = 1
+    while f'l{idx}_weights' in model_data and f'l{idx}_biases' in model_data:
+        idx += 1
+    total_layers = idx - 1
     input_dim = len(model_data.get('l1_weights', [])
                     ) // len(model_data.get('l1_biases', []))
-    output_dim = len(model_data.get('lo_biases', []))
 
     # Helper function to generate the explicit model initialization
     def generate_model_initialization():
         layer_inits = []
-        idx = 1
-
-        while f'l{idx}_weights' in model_data and f'l{idx}_biases' in model_data:
+        for idx in range(1, total_layers + 1):
             output_size = len(model_data.get(f'l{idx}_biases', []))
             input_size = len(model_data.get(
                 f'l{idx}_weights', [])) // output_size
@@ -31,23 +32,6 @@ def generate_nn_code(program_name, model_data):
 
             layer_inits.append(
                 f"{indentation*3}l{idx}: L{idx} {{\n{weights}\n{biases}\n{indentation*3}}},")
-            idx += 1
-
-        # Initialization for output layer
-        output_size = len(model_data.get('lo_biases', []))
-        input_size = len(model_data.get('lo_weights', [])) // output_size
-
-        output_weights = "\n".join(
-            [f"{indentation*4}i{i+1}_o{j+1}: {model_data.get('lo_weights', [])[j*input_size + i]}i128," for j in range(
-                output_size) for i in range(input_size)]
-        )
-        output_biases = "\n".join(
-            [f"{indentation*4}b{j+1}: {model_data.get('lo_biases', [])[j]}i128," for j in range(
-                output_size)]
-        )
-        layer_inits.append(
-            f"{indentation*3}lo: Lo {{\n{output_weights}\n{output_biases}\n{indentation*3}}},")
-
         return "\n".join(layer_inits)
 
     # Helper function to generate layer weights and biases
@@ -57,20 +41,19 @@ def generate_nn_code(program_name, model_data):
                 output_size) for i in range(input_size)]
         )
         biases = "\n".join(
-            [f"{indentation*2}b{j+1}: i128," for j in range(
-                output_size)]
+            [f"{indentation*2}b{j+1}: i128," for j in range(output_size)]
         )
         return f"{indentation}struct L{layer_idx} {{\n{weights}\n{biases}\n{indentation}}}"
 
     # Helper function to generate layer computation
-    def generate_layer_computation(layer_idx, input_size, output_size, prev_layer_idx=None, is_output=False):
+    def generate_layer_computation(layer_idx, input_size, output_size, prev_layer_idx=None):
         if prev_layer_idx:
             inputs = [
                 f"val_l{prev_layer_idx}_{i+1}" for i in range(input_size)]
         else:
             inputs = [f"data.in{i+1}" for i in range(input_size)]
 
-        if is_output:
+        if layer_idx == total_layers:
             computation = "\n".join(
                 [f"{indentation*2}let val_l{layer_idx}_{j+1}: i128 = " +
                  " + ".join([f'model.l{layer_idx}.i{i+1}_o{j+1} * {inputs[i]}' for i in range(input_size)]) +
@@ -82,7 +65,6 @@ def generate_nn_code(program_name, model_data):
                  " + ".join([f'model.l{layer_idx}.i{i+1}_o{j+1} * {inputs[i]}' for i in range(input_size)]) +
                  f" + model.l{layer_idx}.b{j+1});" for j in range(output_size)]
             )
-
         return computation
 
     # Generate the model initialization
@@ -96,13 +78,10 @@ def generate_nn_code(program_name, model_data):
         [f"{indentation*2}in{i+1}: i128," for i in range(input_dim)])
     input_struct += f"\n{indentation}}}"
 
-    # Generate all hidden layer structs and computations based on model_data
+    # Generate all layer structs and computations based on model_data
     layer_structs = []
     layer_computations = []
-    idx = 1
-    prev_layer_size = input_dim
-
-    while f'l{idx}_weights' in model_data and f'l{idx}_biases' in model_data:
+    for idx in range(1, total_layers + 1):
         output_size = len(model_data.get(f'l{idx}_biases', []))
         input_size = len(model_data.get(f'l{idx}_weights', [])) // output_size
 
@@ -111,20 +90,10 @@ def generate_nn_code(program_name, model_data):
         layer_computations.append(generate_layer_computation(
             idx, input_size, output_size, idx-1 if idx > 1 else None))
 
-        prev_layer_size = output_size
-        idx += 1
-
-    # Generate output layer struct and computation
-    output_size = len(model_data.get('lo_biases', []))
-    layer_structs.append(generate_layer_struct(
-        'o', prev_layer_size, output_size))
-    layer_computations.append(generate_layer_computation(
-        'o', prev_layer_size, output_size, idx-1, is_output=True))
-
-    # Argmax computation for output
+    # Argmax computation for final layer
+    output_dim = len(model_data.get(f'l{total_layers}_biases', []))
     conditions = "\n".join(
         [f"{indentation*2}if val_{i+1} > max_val {{\n{indentation*3}max_val = val_{i+1};\n{indentation*3}max_idx = {i+1}u8;\n{indentation*2}}}" for i in range(1, output_dim)])
-
     arg_max_function = f"""
     function arg_max({', '.join([f'val_{i+1}: i128' for i in range(output_dim)])}) -> u8 {{
 {indentation*2}let max_val: i128 = val_1;
@@ -158,7 +127,6 @@ def generate_nn_code(program_name, model_data):
     // Neural Network consisting of multiple layers
     struct NeuralNet {{
 {layers_list},
-        lo: Lo,
     }}
 
     // The main function that takes input and produces an output
@@ -173,10 +141,11 @@ def generate_nn_code(program_name, model_data):
         input_struct=input_struct,
         layer_structs_text="\n".join(layer_structs),
         layers_list=',\n'.join(
-            [f'{indentation*2}l{idx}: L{idx}' for idx in range(1, len(model_data) // 2)]),
+            [f'{indentation*2}l{idx}: L{idx}' for idx in range(1, total_layers + 1)]),
         model_initialization=model_initialization,
         layer_computations_text="\n".join(layer_computations),
-        output_val_list=', '.join([f'val_lo_{i+1}' for i in range(output_dim)])
+        output_val_list=", ".join(
+            [f"val_l{total_layers}_{i+1}" for i in range(output_dim)])
     )
 
     return code
@@ -184,24 +153,21 @@ def generate_nn_code(program_name, model_data):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate Aleo code for a multi layer neural network.")
-    parser.add_argument('--program_name', type=str,
-                        required=True, help="The name of the program.")
-    parser.add_argument('--model_file', type=str,
-                        help="The file containing the model data.")
-    parser.add_argument('--output_file', type=str, default="src/main.leo",
-                        help="The file to write the Aleo code to.")
-
+        description="Generate Aleo code for Neural Network.")
+    parser.add_argument('--model_data', required=True, type=str,
+                        help='Path to JSON file containing model data.')
+    parser.add_argument('--output', default="src/main.leo", type=str,
+                        help='Path to output Aleo code file.')
+    parser.add_argument('--program_name', required=True,
+                        type=str, help='Name of the Aleo program.')
     args = parser.parse_args()
 
-    # Opening JSON file
-    with open(args.model_file) as json_file:
-        model_data = json.load(json_file)
+    with open(args.model_data, 'r') as f:
+        model_data = json.load(f)
 
-    aleo_code = generate_nn_code(
-        args.program_name, model_data)
+    code = generate_nn_code(args.program_name, model_data)
 
-    with open(args.output_file, 'w') as f:
-        f.write(aleo_code)
+    with open(args.output, 'w') as f:
+        f.write(code)
 
-    print(f"Written to {args.output_file}")
+    print(f"Generated Aleo program at {args.output}")
