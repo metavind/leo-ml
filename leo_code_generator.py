72272a4d0615c513ea87399c87bd78b1a6b62cde
metavind <metavind@gmail.com>
2023-08-21 17:21:05 +0200

    feat: remove relu from output layer, remove val_1 if comparison from arg_max, change i32 to i128

--- leo_code_generator.py
+++ leo_code_generator.py
@@ -7,34 +7,42 @@ def generate_nn_code(input_dim, hidden_dims, output_dim):
     # Helper function to generate layer weights and biases
     def generate_layer_struct(layer_idx, input_size, output_size):
         weights = "\n".join(
-            [f"{indentation*2}l{layer_idx}_i{i+1}_o{j+1}: i32," for i in range(
-                input_size) for j in range(output_size)]
+            [f"{indentation*2}l{layer_idx}_i{i+1}_o{j+1}: i128," for j in range(
+                output_size) for i in range(input_size)]
         )
         biases = "\n".join(
-            [f"{indentation*2}l{layer_idx}_b{j+1}: i32," for j in range(
+            [f"{indentation*2}l{layer_idx}_b{j+1}: i128," for j in range(
                 output_size)]
         )
         return f"{indentation}struct L{layer_idx} {{\n{weights}\n{biases}\n{indentation}}}"
 
     # Helper function to generate layer computation
-    def generate_layer_computation(layer_idx, input_size, output_size, prev_layer_idx=None):
+    def generate_layer_computation(layer_idx, input_size, output_size, prev_layer_idx=None, is_output=False):
         if prev_layer_idx:
             inputs = [
                 f"val_l{prev_layer_idx}_{i+1}" for i in range(input_size)]
         else:
             inputs = [f"data.i{i+1}" for i in range(input_size)]
 
-        computation = "\n".join(
-            [f"{indentation*2}let val_l{layer_idx}_{j+1}: i32 = relu(" +
-             " + ".join([f'model.l{layer_idx}.l{layer_idx}_i{i+1}_o{j+1} * {inputs[i]}' for i in range(input_size)]) +
-             f" + model.l{layer_idx}.l{layer_idx}_b{j+1});" for j in range(output_size)]
-        )
+        if is_output:
+            computation = "\n".join(
+                [f"{indentation*2}let val_l{layer_idx}_{j+1}: i128 = " +
+                 " + ".join([f'model.l{layer_idx}.l{layer_idx}_i{i+1}_o{j+1} * {inputs[i]}' for i in range(input_size)]) +
+                 f" + model.l{layer_idx}.l{layer_idx}_b{j+1};" for j in range(output_size)]
+            )
+        else:
+            computation = "\n".join(
+                [f"{indentation*2}let val_l{layer_idx}_{j+1}: i128 = relu(" +
+                 " + ".join([f'model.l{layer_idx}.l{layer_idx}_i{i+1}_o{j+1} * {inputs[i]}' for i in range(input_size)]) +
+                 f" + model.l{layer_idx}.l{layer_idx}_b{j+1});" for j in range(output_size)]
+            )
+
         return computation
 
     # Generate the input struct
     input_struct = f"struct Input {{\n"
     input_struct += "\n".join(
-        [f"{indentation*2}i{i+1}: i32," for i in range(input_dim)])
+        [f"{indentation*2}i{i+1}: i128," for i in range(input_dim)])
     input_struct += f"\n{indentation}}}"
 
     # Generate all hidden layer structs and computations
@@ -52,15 +60,15 @@ def generate_nn_code(input_dim, hidden_dims, output_dim):
     layer_structs.append(generate_layer_struct(
         'o', prev_layer_size, output_dim))
     layer_computations.append(generate_layer_computation(
-        'o', prev_layer_size, output_dim, len(hidden_dims)))
+        'o', prev_layer_size, output_dim, len(hidden_dims), is_output=True))
 
     # Argmax computation for output
     conditions = "\n".join(
-        [f"{indentation*2}if val_{i+1} > max_val {{\n{indentation*3}max_val = val_{i+1};\n{indentation*3}max_idx = {i+1}u8;\n{indentation*2}}}" for i in range(output_dim)])
+        [f"{indentation*2}if val_{i+1} > max_val {{\n{indentation*3}max_val = val_{i+1};\n{indentation*3}max_idx = {i+1}u8;\n{indentation*2}}}" for i in range(1, output_dim)])
 
     arg_max_function = f"""
-    function arg_max({', '.join([f'val_{i+1}: i32' for i in range(output_dim)])}) -> u8 {{
-{indentation*2}let max_val: i32 = val_1;
+    function arg_max({', '.join([f'val_{i+1}: i128' for i in range(output_dim)])}) -> u8 {{
+{indentation*2}let max_val: i128 = val_1;
 {indentation*2}let max_idx: u8 = 1u8;
 {conditions}
 {indentation*2}return max_idx;
@@ -72,11 +80,11 @@ def generate_nn_code(input_dim, hidden_dims, output_dim):
     // Neural Network Multi Layer Model
 
     // Define the ReLU activation function
-    function relu(x: i32) -> i32 {{
-        if x > 0i32 {{
+    function relu(x: i128) -> i128 {{
+        if x > 0i128 {{
             return x;
         }} else {{
-            return 0i32;
+            return 0i128;
         }}
     }}
 
