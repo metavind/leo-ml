c8261739f457f9fa4793c10bef6ca09c33ac62a9
metavind <metavind@gmail.com>
2023-08-22 17:17:11 +0200

    docs: add documentation for leo_test_generator

--- leo_test_generator.py
+++ leo_test_generator.py
@@ -1,3 +1,36 @@
+"""
+Script Generator for Aleo Program Testing
+
+This script provides a utility to generate a bash test script for Aleo programs. The generated test script
+will run the Aleo program with different input values specified in a JSON file and print both the actual
+and expected output values.
+
+The JSON file should have the following structure:
+{
+    "input1": [input_values1],
+    "output1": expected_output1,
+    "input2": [input_values2],
+    "output2": expected_output2,
+    ...
+}
+
+Inputs:
+    - A JSON file containing the test samples. The JSON should have keys of the form 
+      'inputX' and 'outputX', where X is the sample number (starting from 1).
+
+Outputs:
+    Generates a bash script named `test_<program_name>.sh`. This script can be executed
+    to run the tests on the specified Aleo program.
+
+Usage:
+    Run the script and provide the necessary command-line arguments:
+    --program_name: The name of the Aleo program to be tested.
+    --test_samples: Path to the JSON file containing the test samples (input-output pairs).
+
+Example:
+    python leo_test_generator.py --program_name nn --test_samples test_samples.json
+"""
+
 import argparse
 import json
 import os
